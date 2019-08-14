3d01e45a09ee51217680a03e056c24e0dfaa592b
Seth NeJame <sethnejame@gmail.com>
2019-08-14 22:54:35 +0200

    Adding state (tasks) to App.js:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Complete Arbetsprov Task List",
+      completed: false,
+      status: "active"
+    },
+    {
+      id: 2,
+      title: "Drink a beer",
+      completed: false,
+      status: "onhold"
+    },
+    {
+      id: 3,
+      title: "Continue the Job Search",
+      completed: false,
+      status: "onhold"
+    }
+  ]);
+
   return (
     <div>
-     <h1 className="title">Task List</h1>
+      <h1 className="title">Task List</h1>
     </div>
   );
 }
