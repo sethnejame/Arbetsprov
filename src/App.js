b4ea07c00fd3c8e64bc7787396b2d538fb787c77
Seth NeJame <sethnejame@gmail.com>
2019-08-14 23:11:01 +0200

    Adding TaskList state to App, passing props to TaskList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import TaskList from "./components/TaskList";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Complete Arbetsprov TaskList Challenge",
+      status: "active",
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Get a job offer",
+      status: "on hold",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Drink beer",
+      status: "on hold",
+      completed: false
+    }
+  ]);
   return (
     <div>
-     <h1 className="title">Task List</h1>
+      <h1 className="title">Task List</h1>
+      <TaskList tasklist={tasks} />
     </div>
   );
 }
