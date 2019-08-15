b0421ca6851eadfde89dde87ca4af7e8f10b498c
Seth NeJame <sethnejame@gmail.com>
2019-08-15 18:27:11 +0200

    Moving all logic back to App.js, rebuilding tasklist & task components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React, { useState } from "react";
-import TaskList from "./components/TaskList";
 
+// component for individual classes
+function Task({ task, index }) {
+  return <div className="task">{task.title}</div>;
+}
+
+// component for adding tasks to list
+function TaskForm({ addTask }) {
+  // hooks w/ state for new tasks
+  const [value, setValue] = useState("");
+  // submit handler for new tasks
+  const handleSubmit = e => {
+    e.preventDefault(); // prevent form sub & page reload
+    if (!value) return;
+    addTask(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="task"
+        value={value} // state value comes in here
+        onChange={e => setValue(e.target.value)} // use the hook to set the new val
+        placeholder="Add new task here. . ."
+      />
+    </form>
+  );
+}
+
+// HOC component
 function App() {
+  // hooks w/ state for tasks
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -22,10 +53,23 @@ function App() {
       completed: true
     }
   ]);
+
+  // send new task props to TaskForm via addTask
+  const addTask = text => {
+    const newTasks = [...tasks, { title: text }];
+    setTasks(newTasks);
+  };
+
+  // iterate over current tasks here, display in page, inc. addTask
   return (
     <div>
       <h1 className="title">Task List</h1>
-      <TaskList taskList={tasks} />
+      <div className="taskList">
+        {tasks.map((task, index) => (
+          <Task task={task} key={index} index={index} />
+        ))}
+        <TaskForm addTask={addTask} />
+      </div>
     </div>
   );
 }
