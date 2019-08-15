aa61b711dbe2a1b4661aa69a0b79eb15dd2666de
Seth NeJame <sethnejame@gmail.com>
2019-08-15 07:09:26 +0200

    A bit of CSS and adding a TaskListItem component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TaskListItem from "./TaskListItem";
 
 function TaskList(props) {
   return props.tasklist.map(task => <h3>{task.title}</h3>);
---
+++ src/components/TaskListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TaskListItem(props) {
+  return (
+    <div>
+      <p>Wow!</p>
+    </div>
+  );
+}
+
+export default TaskListItem;
