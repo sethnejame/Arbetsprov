de3e285cc7113fe0ed008029dc7f115de30122e5
Seth NeJame <sethnejame@gmail.com>
2019-08-15 08:28:04 +0200

    Adding custom styles for completed, active, pending Tasks

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,13 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-
 function TaskListItem(props) {
   return (
-    <div className="taskListItem">
+    <div className="taskListItem" style={crossOut(props)}>
       {props.taskList.title}
     </div>
   );
+}
+
+function crossOut(props) {
+  if (props.taskList.completed) {
+    return {
+      textDecoration: "line-through"
+    };
+  } else if (props.taskList.status === "active") {
+    return {
+      textDecoration: "underline"
+    };
+  } else {
+    return {
+      textDecoration: "none"
+    };
+  }
 }
 
 TaskListItem.propTypes = {
