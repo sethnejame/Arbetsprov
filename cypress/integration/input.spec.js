041839dddafc30fb79aba358bbd3ec5f6d64f1e5
Seth NeJame <sethnejame@gmail.com>
2019-08-17 09:04:22 +0200

    Adding tests (form submission posts new task to list) and more styling

--- cypress/integration/input.spec.js
+++ cypress/integration/input.spec.js
@@ -1,17 +1,32 @@
-describe('Input form', () => {
-  it('focuses input on load', () => {
-    cy.visit('http://localhost:3000')
+import { isContext } from "vm";
 
+describe('Input form', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+  
+  it('focuses input on load', () => {
     cy.focused()
       .should('have.class', 'taskForm')
   })
 
-  it.only('accepts input', () => {
+  it('accepts input', () => {
     const typedTask = 'Reset the Flux Capacitor'
-    cy.visit('http://localhost:3000')
 
     cy.get('.taskForm')
       .type(typedTask)
       .should('have.value', typedTask)
+  })
+
+  context('Form submission', () => {
+    it.only('Adds new task on submit', () => {
+      const addedTask = 'Tune up the MegaCondenser'
+
+      cy.get('.taskForm')
+        .type(addedTask)
+        .type('{enter}')
+      cy.get('.task')
+        .should('contain', addedTask)
+    })
   })
 })
